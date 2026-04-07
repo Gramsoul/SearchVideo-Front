af39fa8d1f2c4ad2166f7230158a3ad77871be3a
Gonzalo <erebea@live.com.ar>
2026-04-07 19:48:56 -0300

    front testeo actualizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,44 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+
 interface ytdata {
   uploader: string;
   title: string;
   url: string;
   thumbnail: string;
   description?: string;
+  formats: vFormat[];
 }
+
+interface vFormat {
+  ext: string;
+  filesize: number;
+  height: number;
+  format_id: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
   protected readonly title = signal('SearchVideo-Front');
 
   url = signal('')
   data = signal<ytdata | null>(null)
 
-
   search() {
-    console.log(this.data());
-
-    
-    fetch('https://searchvideo-production.up.railway.app/info?url=' + this.url())
+    //fetch('https://searchvideo-production.up.railway.app/info?url=' + this.url())
+    fetch('http://localhost:8080/info?url=' + this.url())
       .then(response => response.json())
       .then(data => {
         this.data.set(data)
       })
-
   }
-
 
 }
