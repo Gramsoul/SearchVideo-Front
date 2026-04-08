bf8e7a5776342c781aa067e153bb622eb1548d0f
Gram <erebea@live.com.ar>
2026-04-08 20:11:47 -0300

    se agrego download()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DecimalPipe } from '@angular/common';
 
 interface ytdata {
   uploader: string;
@@ -22,23 +21,47 @@ interface vFormat {
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-
-
 export class App {
   protected readonly title = signal('SearchVideo-Front');
 
-  url = signal('')
-  data = signal<ytdata | null>(null)
+  url = signal('');
+  data = signal<ytdata | null>(null);
 
   search() {
-    fetch('https://searchvideo-production.up.railway.app/info?url=' + this.url())
-    //fetch('http://localhost:8080/info?url=' + this.url())
-      .then(response => response.json())
-      .then(data => {
-        this.data.set(data)
-      })
+    //fetch('https://searchvideo-production.up.railway.app/info?url=' + this.url())
+    fetch('http://localhost:8080/info?url=' + this.url()) //test
+      .then((response) => response.json())
+      .then((data) => {
+        this.data.set(data);
+      });
+  }
+
+  download_video(){
+    //const api_url = 'http://localhost:8080/download'; //local
+    const api_url = 'https://searchvideo-production.up.railway.app/download'
+    const params = ({
+      url: this.url(),
+      format_id: this.data()?.formats[1].format_id, //debe recibir el formato elegido
+      dir: 'C:/Users/Gram/Desktop/test' //cambiar
+    })
+    const fetch_api = `${api_url}?${params.toString()}`
+
+    fetch(fetch_api);
+  }
+
+  
+  download_test_360() {
+    const api_url = 'http://localhost:8080/download';
+    const param = new URLSearchParams({
+      url: 'https://www.youtube.com/watch?v=q2oGa3AMT_U',
+      format_id: '134',
+      dir: 'C:/Users/Gram/Desktop/test',
+    });
+    const link = `${api_url}?${param.toString()}`;
+    console.log(param);
+    fetch(link)
   }
 
 }
